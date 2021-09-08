f2cf4e771f80428bec6bf54465c7a34f968e417e
amir-saeedi <saeediamir1381@gmail.com>
2021-09-09 01:33:51 +0430

    fix error

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime/runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
