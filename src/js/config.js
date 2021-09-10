73a2221db3e5d163b6039e330c9a7310ad2c6627
amir-saeedi <saeediamir1381@gmail.com>
2021-09-10 15:33:43 +0430

    chenge key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '5cb59c4f-9e54-4934-a40a-55219480b939';
+export const KEY = '75c110c9-23b2-4a69-9037-40879ae86e8c';
 export const MODAL_CLOSE_SEC = 2.5;
